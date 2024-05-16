8a3ce83d31f1c3c22c98da7dc3338c68443221bd
Nikodem Brożyniak <116508040+Niki-3D@users.noreply.github.com>
2024-05-16 21:02:26 +0200

    changes

---
+++ src/app/login_and_register/page.jsx
@@ -0,0 +1,22 @@
+"use client";
+import '../login_and_register/page.scss';
+
+const login_and_register = () => {
+    return (
+        <main className="login_and_register">
+            <div className="container">
+                <div className="login">
+                    <p>Log in Section</p>
+                </div>
+
+                <div className="register">
+                    <p>Register Section</p>
+                </div>
+            </div>
+        </main>
+        
+    );
+  };
+  
+  export default login_and_register;
+  
