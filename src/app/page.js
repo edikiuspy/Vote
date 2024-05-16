afa1011a1696962d4c37926e7b489a273c37707f
edikiuspy <u96_edumun_waw@technischools.com>
2024-05-16 19:59:48 +0200

    work ig

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
-import EmblaCarousel from '../components/embla_carousel/embla_carousel.jsx';
-
+"use client";
+import EmblaCarousel from "../app/components/embla_carousel/embla_carousel";
 
 export default function Home() {
   return (
     <main className="">
-      <EmblaCarousel slides={Array.from(Array(5).keys())} options={{ loop: true }} />
+      <EmblaCarousel
+        slides={Array.from(Array(5).keys())}
+        options={{ loop: true }}
+      />
     </main>
   );
 }
