78ca93c7668723278cd219d332467dea68b60587
edikiuspy <u96_edumun_waw@technischools.com>
2024-05-16 23:09:30 +0200

    well

---
+++ src/app/components/game/game.jsx
@@ -0,0 +1,9 @@
+const Game=(game) => {
+    console.log(game);
+    return (
+        <div>
+        {game.game.name}
+        </div>
+    );
+}
+export default Game;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,19 @@
 "use client";
 import EmblaCarousel from "../app/components/embla_carousel/embla_carousel";
+import Game from "../app/components/game/game";
+async function getGames() {
+  const res = await fetch(`http://localhost:3000/api/game`);
+  const data = await res.json();
+  return data;
+}
 
-export default function Home() {
+export default async function Home() {
+  const games = await getGames();
   return (
     <main className="">
+      <div>
+      {games.map(game => <Game key={game.id} game={game} />)}
+      </div>
       <EmblaCarousel
         slides={Array.from(Array(5).keys())}
         options={{ loop: true }}
