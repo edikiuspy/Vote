38810a3a24a690b66cebac05e559f61b650a14f8
Nikodem Brożyniak <116508040+Niki-3D@users.noreply.github.com>
2024-05-16 23:19:13 +0200

    Navbar initialization

---
+++ src/app/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">Login</Link></li>
+            <li><Link to="/contact">Register</Link></li>
+            <li><Link to="/contact">Company login</Link></li>
+            <li><Link to="/contact">Company register</Link></li>
+        </ul>
+        </nav>
+    );
+    }
+export default Navbar;
+
