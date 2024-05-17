ba4136878454d16cb53a7b4dec3974f07519630b
edikiuspy <u96_edumun_waw@technischools.com>
2024-05-17 11:39:12 +0200

    vell

--- src/app/components/game/game.jsx
+++ src/app/components/game/game.jsx
@@ -1,8 +1,11 @@
+import Button from "./vote.jsx";
 const Game = (game) => {
   return (
     <div>
       <p>{game.game.name}</p>
       <p>{game.game.description}</p>
+      <p>{game.game.votes}</p>
+      <Button />
     </div>
   );
 };
